5246c2ceea147c92cd4f9a6184fd17ef08556df0
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-10-17 20:21:07 +0530

    original commit in main doctors blog clone

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+//Ui unique to route
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      hello
+    </main>
+  )
+}
